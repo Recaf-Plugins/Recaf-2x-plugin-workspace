106e85b5b4386252c80c3e838d78324aa481d15d
Col-E <mcoley2@gmu.edu>
2020-05-01 02:21:27 -0400

    Update recaf dependency

--- src/main/java/me/yourname/pluginname/TemplatePlugin.java
+++ src/main/java/me/yourname/pluginname/TemplatePlugin.java
@@ -4,16 +4,14 @@ import org.plugface.core.annotations.Plugin;
 import me.coley.recaf.plugin.api.*;
 
 @Plugin(name = "Template")
-public class TemplatePlugin implements PluginBase {
-    @Override
-    public String getVersion() {
-        return "1.0";
-    }
+public class TemplatePlugin implements BasePlugin {
+	@Override
+	public String getVersion() {
+		return "1.0";
+	}
 
-    @Override
-    public String getDescription() {
-        return "A template plugin to work off of";
-    }
+	@Override
+	public String getDescription() {
+		return "A template plugin to work off of";
+	}
 }
-
-
