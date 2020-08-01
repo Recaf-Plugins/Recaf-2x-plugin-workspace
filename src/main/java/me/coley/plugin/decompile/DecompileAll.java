829e43170100c355392fbc94adc84631741f53d0
Col-E <mcoley2@gmu.edu>
2020-08-01 04:45:57 -0400

    Fix multiple decompile-all plugin bugs

--- src/main/java/me/coley/plugin/decompile/DecompileAll.java
+++ src/main/java/me/coley/plugin/decompile/DecompileAll.java
@@ -22,7 +22,7 @@ public class DecompileAll implements StartupPlugin, ContextMenuInjectorPlugin {
 
 	@Override
 	public String getVersion() {
-		return "1.0.0";
+		return "1.1.0";
 	}
 
 	@Override
--- src/main/java/me/coley/plugin/decompile/DecompilePanel.java
+++ src/main/java/me/coley/plugin/decompile/DecompilePanel.java
@@ -2,6 +2,7 @@ package me.coley.plugin.decompile;
 
 import javafx.application.Platform;
 import javafx.collections.FXCollections;
+import javafx.scene.control.Label;
 import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
 import javafx.scene.control.ProgressBar;
@@ -21,11 +22,11 @@ import java.io.FileOutputStream;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.StandardOpenOption;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 import java.util.TreeSet;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.concurrent.*;
 import java.util.jar.JarEntry;
 import java.util.zip.ZipOutputStream;
 
@@ -33,9 +34,11 @@ public class DecompilePanel extends BorderPane {
 	private static final Color SUCCESS_COLOR = new Color(0.05, 0.45, 0.05, 1);
 	private static final Color FAIL_COLOR = new Color(0.5, 0.05, 0.05, 1);
 	private final int classCount;
+	private final List<Long> decompileTimes = new CopyOnWriteArrayList<>();
 	private final FileChooser saver = new FileChooser();
 	private final Map<String, String> classSources = new ConcurrentHashMap<>();
 	private final ListView<String> list = new ListView<>();
+	private final Label lblEstimatedtime = new Label("Remaining time: Unknown");
 	private final ProgressBar progressBar = new ProgressBar(0);
 	private final GuiController controller;
 	private final Stage window;
@@ -52,39 +55,58 @@ public class DecompilePanel extends BorderPane {
 		saver.getExtensionFilters().add(filter);
 		saver.setSelectedExtensionFilter(filter);
 		setCenter(list);
-		setBottom(progressBar);
+		BorderPane bottomInfo = new BorderPane();
+		bottomInfo.setCenter(progressBar);
+		bottomInfo.setBottom(lblEstimatedtime);
+		setBottom(bottomInfo);
 		progressBar.setMaxWidth(Double.MAX_VALUE);
 		// Show window
 		window = controller.windows().window("Decompile export", this);
 		window.show();
 		// Start decompile
-		Decompiler<?> decompiler = controller.config().decompile().decompiler.create(controller);
-		ExecutorService service = Executors.newWorkStealingPool();
-		for(String name : strings)
-			service.submit(() -> {
-				try {
-					onDecompile(name, decompiler.decompile(name));
-				} catch(Exception ex) {
-					Log.error(ex, "Class failed to decompiled: {}", name);
-					onDecompile(name, null);
-				}
-			});
-		service.submit(() -> {
-			try {
-				while(classSources.size() < classCount)
-					Thread.sleep(600);
-				Platform.runLater(this::saveSources);
-			} catch(InterruptedException e) {
-				/* Not gonna happen */
-			}
-		});
-		service.shutdown();
+		new Thread(() -> startDecompile(strings)).start();
 	}
 
-	private void onDecompile(String name, String decompiled) {
-		// Update map
-		classSources.put(name, decompiled);
-		// Update UI
+	private void startDecompile(TreeSet<String> strings) {
+		Decompiler<?> decompiler = controller.config().decompile().decompiler.create(controller);
+		int nThreads = Math.max(1, Math.min(4, Runtime.getRuntime().availableProcessors()));
+		ExecutorService service = Executors.newFixedThreadPool(nThreads);
+		for (String name : strings)
+			service.submit(() -> {
+				try {
+					long start = System.currentTimeMillis();
+					classSources.put(name, decompiler.decompile(name));
+					decompileTimes.add(System.currentTimeMillis() - start);
+					updateProgressUI();
+				} catch (Exception ex) {
+					Log.error(ex, "Class failed to decompiled: {}", name);
+					classSources.put(name, "// Failed to decompile: " + name);
+				}
+			});
+		service.shutdown();
+		try {
+			service.awaitTermination(1, TimeUnit.DAYS);
+			Platform.runLater(() -> {
+				saveSources();
+			});
+		} catch (InterruptedException e) {
+			Log.error("Interrupted decompile service");
+		}
+	}
+
+	private void updateProgressUI() {
+		int done = decompileTimes.size();
+		int remaining = classCount - done;
+		decompileTimes.stream().mapToLong(Long::longValue)
+				.average().ifPresent(avgMillis -> {
+			long etaRemaining = (long) (avgMillis * remaining);
+			String hms = String.format("%02d:%02d:%02d", TimeUnit.MILLISECONDS.toHours(etaRemaining),
+					TimeUnit.MILLISECONDS.toMinutes(etaRemaining) % TimeUnit.HOURS.toMinutes(1),
+					TimeUnit.MILLISECONDS.toSeconds(etaRemaining) % TimeUnit.MINUTES.toSeconds(1));
+			Platform.runLater(() -> {
+				lblEstimatedtime.setText("Remaining time: " + hms);
+			});
+		});
 		Platform.runLater(() -> {
 			list.refresh();
 			progressBar.setProgress(classSources.size() / (double) classCount);
@@ -96,17 +118,17 @@ public class DecompilePanel extends BorderPane {
 		File file = saver.showSaveDialog(null);
 		if (file != null) {
 			try {
-				 if (classCount == 1) {
-					 Files.write(file.toPath(), classSources.get(classSources.keySet().iterator().next()).getBytes(StandardCharsets.UTF_8), StandardOpenOption.CREATE);
-				 } else {
-				 	try (ZipOutputStream zos = new ZipOutputStream(new FileOutputStream(file))) {
+				if (classCount == 1) {
+					Files.write(file.toPath(), classSources.get(classSources.keySet().iterator().next()).getBytes(StandardCharsets.UTF_8), StandardOpenOption.CREATE);
+				} else {
+					try (ZipOutputStream zos = new ZipOutputStream(new FileOutputStream(file))) {
 						for (Map.Entry<String, String> e : classSources.entrySet()) {
 							zos.putNextEntry(new JarEntry(e.getKey() + ".java"));
 							zos.write(e.getValue().getBytes(StandardCharsets.UTF_8));
 							zos.closeEntry();
 						}
 					}
-				 }
+				}
 			} catch (Exception ex) {
 				Log.error(ex, "Failed to save sources to file: {}", file.getName());
 				ExceptionAlert.show(ex, "Failed to save application to file: " + file.getName());
